47aa45da6421b4c05aac43e1c00dfbb5d7dada27
Kamilunavo <acciento89@gmail.com>
2026-09-03 11:00:53 +0200

    Compact authenticated navigation

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -14,11 +14,17 @@ export function AppHeader({ name }: { name: string }) {
         <Link href="/app/scannen" className="app-nav-link">QR scannen</Link>
         <Link href="/app/service" className="app-nav-link">Service</Link>
         <Link href="/app/notifications" className="app-nav-link">Hinweise</Link>
-        <Link href="/app/team" className="app-nav-link">Bereiche</Link>
-        <Link href="/app/profil" className="app-nav-link">Profil</Link>
-        <Link href="/app/settings" className="app-nav-link">Einstellungen</Link>
-        <Link href="/preise" className="app-nav-link">Preise</Link>
-        <Link href="/impressum" className="app-nav-link">Rechtliches</Link>
+        <details className="app-nav-more">
+          <summary className="app-nav-link">Mehr</summary>
+          <div className="app-nav-menu">
+            <Link href="/app/team">Bereiche</Link>
+            <Link href="/app/profil">Profil</Link>
+            <Link href="/app/sticker">QR-Aufkleber</Link>
+            <Link href="/app/settings">Einstellungen</Link>
+            <Link href="/preise">Preise</Link>
+            <Link href="/impressum">Rechtliches</Link>
+          </div>
+        </details>
         <Link href="/app/assets/new" className="button small">+ Objekt</Link>
         <form action={logoutAction}><button className="button ghost small" type="submit">Abmelden</button></form>
       </nav>
