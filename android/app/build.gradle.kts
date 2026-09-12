601b0c611123a162b2781816b32c80681af99e9a
Kamilunavo <acciento89@gmail.com>
2026-09-12 14:43:22 +0200

    Bump NavoPass Android icon-safe release to 1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.kamilunavo.navopass"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.3"
+        versionCode = 9
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_URL", "\"https://navopass.de\"")
     }
